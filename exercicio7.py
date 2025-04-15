ab6f15e980c8118cdb7f4bb405b700108bc855bd
Pedro Carreteiro <140073394+PedroCarreteiro@users.noreply.github.com>
2025-04-15 15:53:07 -0300

    Atualização exercicio7.py

--- exercicio7.py
+++ exercicio7.py
@@ -3,7 +3,9 @@ numero = int(input("Digite a quantidade de itens total: "))
 
 if numero >= 0:
     while i <= numero:
-        print(f"{"*"*i}")
+        asterisco_unidade = "*"
+        asterisco = asterisco_unidade * i
+        print(f"{asterisco}")
         i += 1
 else:
     print("INVALIDO")
